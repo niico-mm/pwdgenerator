e932fc83b0e4ee57c793e30dcc928f8b39d80bd1
松原恵 <mm.cosmos202@gmail.com>
2016-08-14 13:35:09 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('./') // 公開したい静的ファイルを配置したディレクトリを指定する
+    .pipe(webserver({
+      host: 'localhost',
+      port: 8000,
+      livereload: true
+    }));
+});
+
+gulp.task('default', ['webserver']);
